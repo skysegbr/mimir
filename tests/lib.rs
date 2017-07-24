2ee35c7bc12525fa93675f4609f2e73e80c0b0ff
Jason Ozias <jason.g.ozias@gmail.com>
2017-07-24 11:10:14 -0400

    .travis.yml

--- tests/lib.rs
+++ tests/lib.rs
@@ -17,25 +17,31 @@ mod objecttype;
 mod pool;
 mod statement;
 
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[cfg(test)]
 lazy_static! {
     pub static ref CREDS: Vec<String> = {
-        let file = File::open(".creds/oic-test")
-            .expect("bad creds");
-        let buf_reader = BufReader::new(file);
         let mut creds = Vec::new();
+        if let Ok(file) = File::open(".creds/oic-test") {
+            let buf_reader = BufReader::new(file);
 
-        #[cfg_attr(feature = "cargo-clippy", allow(used_underscore_binding))]
-        for line_res in buf_reader.lines() {
-            if let Ok(line) = line_res {
-                let parts = line.split(':').map(|x| {
-                    x.trim_right().to_string()
-                }).collect::<Vec<String>>();
-                creds.extend(parts);
+            #[cfg_attr(feature = "cargo-clippy", allow(used_underscore_binding))]
+            for line_res in buf_reader.lines() {
+                if let Ok(line) = line_res {
+                    let parts = line.split(':').map(|x| {
+                        x.trim_right().to_string()
+                    }).collect::<Vec<String>>();
+                    creds.extend(parts);
+                }
             }
+        } else {
+            let username = env::var("MIMIR_USERNAME").expect("invalid username");
+            let password = env::var("MIMIR_PASSWORD").expect("invalid password");
+            creds.push(username);
+            creds.push(password);
         }
         creds
     };
